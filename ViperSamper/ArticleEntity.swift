ca290226707f6b53aa70fddc7d449e004cb7b801
Wataru Fujiwara <wataru930@gmail.com>
2023-01-24 17:25:00 +0900

    add : entity

---
+++ ViperSamper/ArticleEntity.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleEntity.swift
+//  ViperSamper
+//
+//  Created by Wataru Fujiwara on 2023/01/18.
+//
+
+import Foundation
+
+// https://jsonplaceholder.typicode.com/posts
+struct ArticleEntity {
+    let id: Int
+    let userId: Int
+    let title: String
+    let body: String
+}
