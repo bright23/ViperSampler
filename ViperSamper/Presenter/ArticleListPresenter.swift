c28e6d32ed4b171475ee66b65a0f09683ca09983
Wataru Fujiwara <wataru930@gmail.com>
2023-01-24 17:46:31 +0900

    add : Presenter protocols

--- ViperSamper/Presenter/ArticleListPresenter.swift
+++ ViperSamper/Presenter/ArticleListPresenter.swift
@@ -7,12 +7,22 @@
 
 import Foundation
 
+// View側から送られ、Presenter側で受けるイベント
 protocol ArticleListPresenterProtocol: AnyObject {
-
+    // ViewがLoadされたらAPI通信して値を取りたいので、ViewがLoadされたとき
+    func didLoad()
+    // 一覧から記事を選択したとき
+    func didSelect(articleEntity: ArticleEntity)
 }
 
+// Presenter側からView側へ送るべきイベント
 protocol ArticleListViewProtocol: AnyObject {
-
+    // 取得してきた配列データを画面に表示してもらう
+    func showArticles(_ articleEntities: [ArticleEntity])
+    // データがありませんでした
+    func showEmpty()
+    // エラーが発生しました
+    func showError(_ error: Error)
 }
 
 class ArticleListPresenter {
